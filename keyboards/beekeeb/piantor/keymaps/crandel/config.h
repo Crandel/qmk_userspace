9d99259a91d60a2d8fd781e01c87754d5cc119c3
Vitalii Drevenchuk <4005032+Crandel@users.noreply.github.com>
2024-03-14 14:00:48 +0100

    Add piantor crandel keymap

---
+++ keyboards/beekeeb/piantor/keymaps/crandel/config.h
@@ -0,0 +1,13 @@
+#define VIAL_KEYBOARD_UID {0xDC, 0x6D, 0x9C, 0x42, 0x53, 0x42, 0x75, 0x0F}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 5 }
+#define VIAL_TAP_DANCE_ENTRIES 5
+#define VIAL_COMBO_ENTRIES     15
+
+/* default layer count */
+#define DYNAMIC_KEYMAP_LAYER_COUNT 10
+#define DYNAMIC_KEYMAP_MACRO_COUNT 15
+
+/* Select hand configuration */
+#define MASTER_LEFT
---
+++ keyboards/beekeeb/piantor/keymaps/crandel/rules.mk
@@ -0,0 +1,7 @@
+OLED_ENABLE            = no
+QMK_SETTINGS           = yes
+RAW_ENABLE             = no
+RGBLIGHT_ENABLE        = no
+SPACE_CADET_ENABLE     = no
+SWAP_HANDS_ENABLE      = no      # Enable one-hand typing
+VIRTSER_ENABLE         = no

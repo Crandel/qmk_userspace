d5b26f0b63b58c25990328dab4264e648142ee5f
Vitalii Drevenchuk <4005032+Crandel@users.noreply.github.com>
2024-03-16 19:03:05 +0100

    crkbd update

--- keyboards/beekeeb/piantor/keymaps/crandel/config.h
+++ keyboards/beekeeb/piantor/keymaps/crandel/config.h
@@ -1,7 +1,27 @@
-#define VIAL_KEYBOARD_UID {0xDC, 0x6D, 0x9C, 0x42, 0x53, 0x42, 0x75, 0x0F}
+/*
+Copyright 2019 @foostan
+Copyright 2020 Drashna Jaelre <@drashna>
+Copyright 2023 Vitalii Drevenchuk <@Crandel>
 
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 5 }
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xDC, 0x6D, 0x9C, 0x42, 0x53, 0x42, 0x75, 0x0F}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 2 }
 #define VIAL_TAP_DANCE_ENTRIES 5
 #define VIAL_COMBO_ENTRIES     15
 

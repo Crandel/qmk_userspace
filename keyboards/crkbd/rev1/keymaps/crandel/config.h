f9472f488b7dd4022c147713db6d4c8e14a04ffe
Vitalii Drevenchuk <4005032+Crandel@users.noreply.github.com>
2024-03-18 09:57:36 +0100

    crkbd shift Layout

--- keyboards/crkbd/rev1/keymaps/crandel/config.h
+++ keyboards/crkbd/rev1/keymaps/crandel/config.h
@@ -24,11 +24,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VIAL_UNLOCK_COMBO_ROWS {0, 2}
 #define VIAL_UNLOCK_COMBO_COLS {0, 2}
 #define VIAL_TAP_DANCE_ENTRIES 5
-#define VIAL_COMBO_ENTRIES     15
+#define VIAL_COMBO_ENTRIES     30
 
 /* default layer count */
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
-#define DYNAMIC_KEYMAP_MACRO_COUNT 15
+#define DYNAMIC_KEYMAP_MACRO_COUNT 5
 
 //#define USE_MATRIX_I2C
 #ifdef KEYBOARD_crkbd_rev1_legacy

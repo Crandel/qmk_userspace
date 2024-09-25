69caae843317472b8d6b93c6333bbd162ca7dedb
Vitalii Drevenchuk <4005032+Crandel@users.noreply.github.com>
2024-09-25 10:40:25 +0200

    Update piantor

--- keyboards/beekeeb/piantor/keymaps/crandel/config.h
+++ keyboards/beekeeb/piantor/keymaps/crandel/config.h
@@ -20,8 +20,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 #define VIAL_KEYBOARD_UID {0xDC, 0x6D, 0x9C, 0x42, 0x53, 0x42, 0x75, 0x0F}
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_ROWS { 1, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 2 }
 #define VIAL_TAP_DANCE_ENTRIES 5
 #define VIAL_COMBO_ENTRIES     30
 
